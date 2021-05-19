5fd1dd6497601a507dcdd91f4dedfd2948162c93
Vladimir Pedchenko <paralogyX@gmail.com>
2021-05-19 12:01:27 +0200

    Initial commit

---
+++ data-import.R
@@ -0,0 +1,13 @@
+
+
+path <- system.file("extdata", package = "dslabs")
+#list.files(path)
+
+filename <- "murders.csv"
+fullpath <- file.path(path, filename)
+
+file.copy(fullpath, "./data/")
+
+file.exists(file.path("./data/", filename))
+
+            
