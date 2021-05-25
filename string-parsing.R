2a3612e0293d15652c9b74dbe269577d26e08eda
Vladimir Pedchenko <paralogyX@gmail.com>
2021-05-25 14:39:33 +0200

    3.1: String Processing Part 1 Completed

---
+++ string-parsing.R
@@ -0,0 +1,38 @@
+library(tidyverse) 
+library(rvest)
+
+# read in raw murders data from Wikipedia
+url <- "https://en.wikipedia.org/w/index.php?title=Gun_violence_in_the_United_States_by_state&direction=prev&oldid=810166167"
+murders_raw <- read_html(url) %>% 
+  html_nodes("table") %>% 
+  html_table() %>%
+  .[[1]] %>%
+  setNames(c("state", "population", "total", "murder_rate"))
+
+# inspect data and column classes
+head(murders_raw)
+class(murders_raw$population)
+class(murders_raw$total)
+
+
+s <- "Hello!"    # double quotes define a string
+s <- 'Hello!'    # single quotes define a string
+s <- `Hello`    # backquotes do not
+
+#s <- "10""    # error - unclosed quotes
+s <- '10"'    # correct
+
+# cat shows what the string actually looks like inside R
+cat(s)
+
+s <- "5'"
+cat(s)
+
+# to include both single and double quotes in string, escape with \
+#s <- '5'10"'    # error
+#s <- "5'10""    # error
+s <- '5\'10"'    # correct
+cat(s)
+s <- "5'10\""    # correct
+cat(s)
+
