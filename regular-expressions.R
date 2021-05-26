9c36b95af6df4d362187b1aba3abc11e801b4cb6
Vladimir Pedchenko <paralogyX@gmail.com>
2021-05-26 16:20:00 +0200

    Second part of string parsing lesson

---
+++ regular-expressions.R
@@ -0,0 +1,122 @@
+# load stringr through tidyverse
+library(tidyverse)
+library(rvest)
+
+# read in raw murders data from Wikipedia
+url <- "https://en.wikipedia.org/w/index.php?title=Gun_violence_in_the_United_States_by_state&direction=prev&oldid=810166167"
+murders_raw <- read_html(url) %>% 
+  html_nodes("table") %>% 
+  html_table() %>%
+  .[[1]] %>%
+  setNames(c("state", "population", "total", "murder_rate"))
+
+# detect whether a comma is present
+pattern <- ","
+str_detect(murders_raw$total, pattern) 
+
+# show the subset of strings including "cm"
+str_subset(reported_heights$height, "cm")
+
+# use the "or" symbol inside a regex (|)
+yes <- c("180 cm", "70 inches")
+no <- c("180", "70''")
+s <- c(yes, no)
+str_detect(s, "cm") | str_detect(s, "inches")
+str_detect(s, "cm|inches")
+
+# highlight the first occurrence of a pattern
+str_view(s, pattern)
+#test
+str_view(murders_raw$total, pattern, match = T)
+str_view_all(murders_raw$total, pattern, match = T)
+
+str_view(reported_heights$height, "cm", match = T)
+str_view_all(reported_heights$height, "cm", match = T)
+
+str_view(murders_raw$population, pattern, match = T)
+str_view_all(murders_raw$population, pattern, match = T)
+
+# highlight all instances of a pattern
+str_view_all(s, pattern)
+
+
+# s was defined in the previous video
+yes <- c("5", "6", "5'10", "5 feet", "4'11")
+no <- c("", ".", "Five", "six")
+s <- c(yes, no)
+pattern <- "\\d"
+
+# [56] means 5 or 6
+str_view(s, "[56]")
+
+# [4-7] means 4, 5, 6 or 7
+yes <- as.character(4:7)
+no <- as.character(1:3)
+s <- c(yes, no)
+str_detect(s, "[4-7]")
+
+# ^ means start of string, $ means end of string
+pattern <- "^\\d$"
+yes <- c("1", "5", "9")
+no <- c("12", "123", " 1", "a4", "b")
+s <- c(yes, no)
+str_view(s, pattern)
+
+# curly braces define quantifiers: 1 or 2 digits 
+pattern <- "^\\d{1,2}$"
+yes <- c("1", "5", "9", "12")
+no <- c("123", "a4", "b")
+str_view(c(yes, no), pattern)
+
+# combining character class, anchors and quantifier
+pattern <- "^[4-7]'\\d{1,2}\"$"
+yes <- c("5'7\"", "6'2\"",  "5'12\"")
+no <- c("6,2\"", "6.2\"","I am 5'11\"", "3'2\"", "64")
+str_detect(yes, pattern)
+str_detect(no, pattern)
+
+
+
+# number of entries matching our desired pattern
+pattern <- "^[4-7]'\\d{1,2}\"$"
+sum(str_detect(problems, pattern))
+
+# inspect examples of entries with problems
+problems[c(2, 10, 11, 12, 15)] %>% str_view(pattern)
+str_subset(problems, "inches")
+str_subset(problems, "''")
+
+# replace or remove feet/inches words before matching
+pattern <- "^[4-7]'\\d{1,2}$"
+problems %>% 
+  str_replace("feet|ft|foot", "'") %>% # replace feet, ft, foot with ' 
+  str_replace("inches|in|''|\"", "") %>% # remove all inches symbols
+  str_detect(pattern) %>% 
+  sum()
+
+# R does not ignore whitespace
+identical("Hi", "Hi ")
+
+# \\s represents whitespace
+pattern_2 <- "^[4-7]'\\s\\d{1,2}\"$"
+str_subset(problems, pattern_2)
+
+# * means 0 or more instances of a character
+yes <- c("AB", "A1B", "A11B", "A111B", "A1111B")
+no <- c("A2B", "A21B")
+str_detect(yes, "A1*B")
+str_detect(no, "A1*B")
+
+# test how *, ? and + differ
+data.frame(string = c("AB", "A1B", "A11B", "A111B", "A1111B"),
+           none_or_more = str_detect(yes, "A1*B"),
+           nore_or_once = str_detect(yes, "A1?B"),
+           once_or_more = str_detect(yes, "A1+B"))
+
+# update pattern by adding optional spaces before and after feet symbol
+pattern <- "^[4-7]\\s*'\\s*\\d{1,2}$"
+problems %>% 
+  str_replace("feet|ft|foot", "'") %>% # replace feet, ft, foot with ' 
+  str_replace("inches|in|''|\"", "") %>% # remove all inches symbols
+  str_detect(pattern) %>% 
+  sum()
